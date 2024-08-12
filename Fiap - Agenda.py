624dfb8e3d4a2e4614798858e44ba32a6511e09a
Fred Alexis Belz Hesse <93265853+fredabhesse@users.noreply.github.com>
2024-08-12 14:44:34 -0300

    Projeto de agenda FIAP
    Agenda em Python para cadastro, edição, consulta e exclusão de contatos.

---
+++ Fiap - Agenda.py
@@ -0,0 +1,123 @@
+# DESCRIÇÃO: Projeto de nivelamento em linguagens de computação, o objetivo é criar uma agenda que possua menu com as
+# opções de cadastro, edição, pesquisa, listagem e exclusão de contatos.
+
+# definindo funções necessárias
+
+def get_user():
+    lista = []
+    for elem in listaUsuario:
+        lista.append(elem[0])
+    try:
+        index = lista.index(input("Digite o nome do usuário procurado:"))
+    except ValueError:
+        print("Usuário não existe.")
+        return get_user()
+    return index
+
+
+def catch_user():
+    index = get_user()
+    i = listaUsuario[index]
+    print(f"Nome.....:{i[0]}\nCelular..:{i[1]}\nE-mail...:{i[2]}\n--------------------------")
+    return index
+
+
+def wait_prompt():
+    input("Pressione qualquer tecla para continuar")
+    print("\n" * 30)
+    menu()
+
+
+def menu():
+    select = int(input("**********M E N U **********\n1 - Adicionar novo contato\n2 - Editar contato\n3 - Pesquisar "
+                       "contato\n4 - Lista de contatos\n5 - Apagar um contato\n6 - Sair\nEscolha um opção:_"))
+    if select == 1:  # Adicionar
+        print("\n" * 30)
+        add_user()
+    elif select == 2:  # Editar
+        print("\n" * 30)
+        edit_user()
+    elif select == 3:  # Pesquisar
+        print("\n" * 30)
+        print("---------MODO DE PESQUISA")
+        catch_user()
+        wait_prompt()
+        menu()
+    elif select == 4:  # Listar
+        print("\n" * 30)
+        list_users()
+    elif select == 5:  # Deletar
+        print("\n" * 30)
+        print("---------MODO DE EXCLUSÃO")
+        delete_user()
+    elif select == 6:  # Sair
+        print("\n" * 30)
+        print("OBRIGADO POR UTILIZAR NOSSA AGENDA")
+    else:
+        print("\n" * 30)
+        wait_prompt()
+
+
+def add_user():
+    print("---------CADASTRO DE NOVO USUÁRIO:")
+    nome = str(input("Nome.....:"))
+    celular = str(input("Celular..:"))
+    email = str(input("E-mail...:"))
+    listaUsuario.append([nome, celular, email])
+    print("\n" * 3)
+    print(f"Usuário {nome} adicionado com sucesso!")
+    wait_prompt()
+
+
+def edit_user():
+    print("---------MODO DE EDIÇÃO")
+    index = catch_user()
+    updatefield = int(input("\nSelecione o dado que gostaria de editar\n1 - Nome\n2 - Celular\n3 - E-mail\nOpção "
+                            "desejada_"))
+    if updatefield == 1:
+        print(f"\nNome.....:{listaUsuario[index][0]}")
+        listaUsuario[index][0] = str(input("Altere o nome do usuário: "))
+    elif updatefield == 2:
+        print(f"\nCelular..:{listaUsuario[index][1]}")
+        listaUsuario[index][1] = str(input("Altere o celular do usuário: "))
+    elif updatefield == 3:
+        print(f"\nE-mail...:{listaUsuario[index][2]}")
+        listaUsuario[index][2] = str(input("Altere o e-mail do usuário: "))
+    else:
+        print("Opção inválida")
+        edit_user()
+    print("\n\nDado alterado com sucesso, dados atualizados do usuário:")
+    i = listaUsuario[index]
+    print(f"Nome.....:{i[0]}\nCelular..:{i[1]}\nE-mail...:{i[2]}\n--------------------------")
+    wait_prompt()
+    return listaUsuario
+
+
+def delete_user():
+    index = catch_user()
+    option = (str(input("Deletar o usuário selecionado? Digite S para SIM, N para não")))
+    while option not in ['S', 's', 'N', 'n']:
+        print("Opção digitada inválida")
+        option = (str(input("Deletar o usuário selecionado? Digite S para SIM, N para não")))
+    if option in ['S', 's']:
+        del listaUsuario[index]
+        print("USUÁRIO DELETADO")
+        wait_prompt()
+    else:
+        print("\n" * 30)
+        print("Exclusão cancelada.")
+        wait_prompt()
+
+
+def list_users():
+    print("---------CONTATOS DA AGENDA:")
+    for elem in listaUsuario:
+        i = elem
+        print(f"Nome.....:{i[0]}\nCelular..:{i[1]}\nE-mail...:{i[2]}\n--------------------------")
+    wait_prompt()
+
+
+listaUsuario = [["Edson", "119543-5567", "eds@hotmail.com"],
+                ["Maria", "1193322-3456", "maria@gmail.com"],
+                ["Adriano", "1198786-8584", "adriano@ig.com.br"]]
+menu()
